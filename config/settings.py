29d177092a7e3d244c44986472624b395eee90e9
Paulina Pacura <riwuko@gmail.com>
2020-09-22 15:43:48 +0200

    [2] added user-account app

--- config/settings.py
+++ config/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
 
     'kanban',
+    'user_account',
 ]
 
 MIDDLEWARE = [
