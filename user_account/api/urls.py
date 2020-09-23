594072d852e3b8bed3fb16fc3e12f6b12cc7eccf
Paulina Pacura <riwuko@gmail.com>
2020-09-23 13:28:26 +0200

    [4] added user login view to urls

--- user_account/api/urls.py
+++ user_account/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user_account.api.views import UserCreateView
+from user_account.api.views import UserCreateView, UserLoginView
 
 app_name = "user_account"
 
 urlpatterns = [
     path("register", UserCreateView.as_view(), name="register"),
+    path("login", UserLoginView.as_view(), name="login"),
 ]
