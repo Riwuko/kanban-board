5a15ebc52b676f8475185e7cd52eda8446ec87af
Paulina Pacura <riwuko@gmail.com>
2020-09-24 11:32:43 +0200

    [4] fixed superuser creation

--- user_account/admin.py
+++ user_account/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 
 from user_account.models.user_account import UserAccount
 
-
-class UserAdmin(UserAdmin):
-    model = UserAccount
-    list_display = [
-        "email",
-    ]
-
-
-admin.site.register(UserAccount, UserAdmin)
+admin.site.register(UserAccount)
--- user_account/models/user_account.py
+++ user_account/models/user_account.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-from user_account.models.account_manager import AccountManager
+from user_account.account_manager import AccountManager
 
 
 class UserAccount(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    username = models.CharField(max_length=30, unique=True, null=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
