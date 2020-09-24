6d3dc465aec4780d7a79a7011670adeb4c32bc3a
Paulina Pacura <riwuko@gmail.com>
2020-09-24 14:27:34 +0200

    [4] fixed superuser creation

--- user_account/account_manager.py
+++ user_account/account_manager.py
@@ -2,17 +2,19 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class AccountManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("Email can't be empty")
 
-        user = self.model(email=self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password)
-        user.is_admin = True
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        user = self.create_user(email, password, **extra_fields)
         user.save()
         return user
--- user_account/models/user_account.py
+++ user_account/models/user_account.py
@@ -6,8 +6,10 @@ from user_account.account_manager import AccountManager
 
 class UserAccount(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    is_admin = models.BooleanField(default=False)
+
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
 
@@ -15,3 +17,9 @@ class UserAccount(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
