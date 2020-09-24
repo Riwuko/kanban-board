2aadb43508bb971990c471b6f75cc6c7e94398e3
Paulina Pacura <riwuko@gmail.com>
2020-09-24 09:55:21 +0200

    [4] added init file

--- user_account/admin.py
+++ user_account/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 from user_account.models.user_account import UserAccount
 
-admin.site.register(UserAccount)
+
+class UserAdmin(UserAdmin):
+    model = UserAccount
+    list_display = [
+        "email",
+    ]
+
+
+admin.site.register(UserAccount, UserAdmin)
