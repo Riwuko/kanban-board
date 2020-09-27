99e1cf1311b1b727e89bcb2a0eb91e8c4f4ccc26
Paulina Pacura <riwuko@gmail.com>
2020-09-27 18:08:28 +0200

    [7] url for social login added

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("user_account.api.urls", "user_account_api")),
+    path("social/", include("social_django.urls", namespace="social")),
 ]
