821be151494a148a71addedc96c04abd1ad5ed95
Paulina Pacura <riwuko@gmail.com>
2020-09-29 12:10:34 +0200

    [11] added issue urls

--- kanban/api/urls.py
+++ kanban/api/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
+from kanban.api.views.issue import IssueViewSet
 from kanban.api.views.project import ProjectViewSet
 
 app_name = "kanban"
+
+issue_detail = IssueViewSet.as_view(
+    {
+        "get": "retrieve",
+        "put": "update",
+    }
+)
+
+issue_create = IssueViewSet.as_view(
+    {
+        "post": "create",
+    }
+)
 
 project_list = ProjectViewSet.as_view(
     {
@@ -19,4 +33,6 @@ project_detail = ProjectViewSet.as_view(
 urlpatterns = [
     path("projects/", project_list, name="project-list"),
     path("projects/<int:pk>/", project_detail, name="project-detail"),
+    path("issues/<int:pk>/", issue_detail, name="issue-detail"),
+    path("issues/create/", issue_create, name="issue-create"),
 ]
