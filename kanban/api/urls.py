19b70e02e30dcf7e5a315c1c9552748e6cc3577c
Paulina Pacura <riwuko@gmail.com>
2020-10-01 14:08:57 +0200

    [13] changed urls system

--- kanban/api/urls.py
+++ kanban/api/urls.py
@@ -1,38 +1,12 @@
 from django.urls import path
 from kanban.api.views.issue import IssueViewSet
 from kanban.api.views.project import ProjectViewSet
+from rest_framework import routers
 
 app_name = "kanban"
 
-issue_detail = IssueViewSet.as_view(
-    {
-        "get": "retrieve",
-        "put": "update",
-    }
-)
+router = routers.SimpleRouter()
+router.register(r"projects", ProjectViewSet)
+router.register(r"issues", IssueViewSet)
 
-issue_create = IssueViewSet.as_view(
-    {
-        "post": "create",
-    }
-)
-
-project_list = ProjectViewSet.as_view(
-    {
-        "get": "list",
-        "post": "create",
-    }
-)
-project_detail = ProjectViewSet.as_view(
-    {
-        "get": "retrieve",
-        "put": "update",
-    }
-)
-
-urlpatterns = [
-    path("projects/", project_list, name="project-list"),
-    path("projects/<int:pk>/", project_detail, name="project-detail"),
-    path("issues/<int:pk>/", issue_detail, name="issue-detail"),
-    path("issues/create/", issue_create, name="issue-create"),
-]
+urlpatterns = router.urls
