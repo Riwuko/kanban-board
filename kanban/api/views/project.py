4481a8bad46878efa5b369340104531e0f41ab82
Paulina Pacura <riwuko@gmail.com>
2020-10-07 15:04:10 +0200

    [13] refactored assigning users and issues to project

--- kanban/api/views/project.py
+++ kanban/api/views/project.py
@@ -12,6 +12,8 @@ from rest_framework.response import Response
 from user_account.api.serializers import AssignUserSerializer
 from user_account.models import UserAccount
 from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -26,45 +28,43 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
         return super(ProjectViewSet, self).get_serializer_class()
 
-    # def get_queryset(self):
-    #     return self.queryset.filter(
-    #         Q(owner=self.request.user) | Q(users__pk=self.request.user.pk)
-    #     )
 
-    # def get_project_users(self, request, pk):
-    #     project = self.get_object()
-    #     users = project.users.values("email")
-    #     return Response({"Project users": users})
+class ProjectUsers(generics.ListCreateAPIView):
+    queryset = UserAccount.objects.all()
+    serializer_class = AssignUserSerializer
+    permission_classes = [AllowAny]
 
-    # @action(
-    #     detail=True,
-    #     methods=["get", "post"],
-    #     url_path="issues",
-    #     url_name="project-issues",
-    # )
-    # def project_issues(self, request, pk=None):
-    #     self.serializer_class = IssueSerializer
-    #     if request.method == "GET":
-    #         return self.get_project_issues(request, pk)
-    #     if request.method == "POST":
-    #         return self.set_project_issues(request, pk)
+    def get_queryset(self):
+        project_pk = self.kwargs["pk"]
+        return self.queryset.filter(projects__pk=project_pk)
 
-    # def set_project_issues(self, request, pk):
-    #     project = self.get_object()
-    #     issue, created = Issue.objects.get_or_create(
-    #         title=request.data.get("title"),
-    #         description=request.data.get("description"),
-    #         status=request.data.get("status", Issue.TODO),
-    #         due_date=request.data.get("due_date"),
-    #         project=project,
-    #         owner=self.request.user,
-    #     )
-    #     if created:
-    #         project.project_issues.add(issue)
-    #         return Response({"status": "Issue added to project."})
-    #     return Response({"status": "Issue already assigned to project."})
+    def post(self, request, *args, **kwargs):
+        project = get_object_or_404(Project, id=self.kwargs.get("pk"))
+        user_email = request.data.get("email")
+        user = get_object_or_404(UserAccount, email=user_email)
+        project.users.add(user)
+        return Response({"users": user.email})
 
-    # def get_project_issues(self, request, pk):
-    #     project = self.get_object()
-    #     issues = project.project_issues.values("title")
-    #     return Response({"Project users": issues})
+
+class ProjectIssues(generics.ListCreateAPIView):
+    queryset = Issue.objects.all()
+    serializer_class = IssueSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        project_pk = self.kwargs["pk"]
+        return self.queryset.filter(project__pk=project_pk)
+
+    def post(self, request, *args, **kwargs):
+        project = get_object_or_404(Project, id=self.kwargs.get("pk"))
+        issue, created = Issue.objects.get_or_create(
+            title=request.data.get("title"),
+            description=request.data.get("description"),
+            status=request.data.get("status", Issue.TODO),
+            due_date=request.data.get("due_date"),
+            project=project,
+            owner=self.request.user,
+        )
+
+        project.project_issues.add(issue)
+        return Response({"issue": issue.title})
