1b2d33048539991046d4a861dc495cee1c4fefbf
Paulina Pacura <riwuko@gmail.com>
2020-10-25 01:05:27 +0200

    [3] fixed registration page

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -1,8 +1,28 @@
 import React from "react";
-import SignUpForm from "./SignUpForm";
+import { Flex, Image } from "@chakra-ui/core";
+import SignUpPage from "./SignUpForm";
+import background from "../styles/img/border.jpeg";
 
 const Registration = () => {
-  return <SignUpForm />;
+  return (
+    <Flex justifyContent="center" alignItems="center" minHeight="100vh">
+      <Flex
+        height={["90vh", "90vh", "90vh", "70vh"]}
+        width={["100vw", "100vw", "100vw", "80vw", "30vw"]}
+        backgroundColor="white"
+        borderRadius={["20px", "20px", "20px", "20", "20px 0px 0px 20px"]}
+      >
+        <SignUpPage />
+      </Flex>
+      <Flex height="70vh" display={["none", "none", "none", "none", "flex"]}>
+        <Image
+          src={background}
+          borderRadius={"0px 20px 20px 0px"}
+          objectFit="cover"
+        />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Registration;
--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,44 +1,58 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import {
-  Button,
-  CSSReset,
-  Checkbox,
-  Input,
-  ThemeProvider,
-} from "@chakra-ui/core";
-import { ErrorMessage } from "@hookform/error-message";
+import { Heading, Box, Flex, Input, Stack } from "@chakra-ui/core";
+import githubLogo from "../styles/img/google_app_icon.png";
+import googleLogo from "../styles/img/github_app_icon.png";
+import { CheckboxForm, ButtonSubmit, ImageLogo, TextError } from "./CustomElements";
+
+const SignUpPage = () => {
+  return (
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      padding="1rem"
+      width="100%"
+    >
+      <Heading marginBottom="3rem">Sign up</Heading>
+      <Box width={["auto", "auto", "auto", "50vw", "22vw"]}>
+        <SignUpForm />
+        <SocialLogin />
+      </Box>
+    </Flex>
+  );
+};
 
 const SignUpForm = () => {
-  const { register, errors, watch, handleSubmit } = useForm();
+  const { handleSubmit, register, errors, watch } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <ThemeProvider>
-      <CSSReset />
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={5}>
         <Input
           name="email"
-          ref={register({
-            minLength: 8,
-          })}
+          type="text"
+          ref={register}
           placeholder="Email"
           required
         />
-
+        {errors.password && <TextError text="Password too short" />}
         <Input
           name="password"
-          type="password"
-          ref={register}
+          type={"password"}
+          ref={register({
+            minLength: 8,
+            message: "Password too short",
+          })}
           placeholder="Password"
           required
         />
-
+        {errors.newPassword && <TextError text="Passwords don't match" />}
         <Input
-          type="text"
           name="newPassword"
           type="password"
           ref={register({
@@ -47,25 +61,32 @@ const SignUpForm = () => {
           placeholder="Repeat password"
           required
         />
-        <ErrorMessage
-          errors={errors}
-          name="newPassword"
-          message="Passwords don't match"
-        />
-
-        <Checkbox
+        {errors.agreement && <TextError text={errors.agreement.message} />}
+        <CheckboxForm
           name="agreement"
+          variantColor="green"
+          text="I agree to the Terms of Service"
           ref={register({
             required: "You have to accept the terms",
           })}
-        >
-          I agree to the Terms of Service
-        </Checkbox>
-
-        <Button type="submit">Send</Button>
-      </form>
-    </ThemeProvider>
+        />
+        <ButtonSubmit type="submit" text="Submit" />
+      </Stack>
+    </form>
   );
 };
 
-export default SignUpForm;
+const SocialLogin = () => {
+  return (
+    <Flex
+      marginTop={["0", "0", "0", "1rem"]}
+      justifyContent="space-around"
+      width="100%"
+    >
+      <ImageLogo src={githubLogo} alt="Google Logo" text="Google Login" />
+      <ImageLogo src={googleLogo} alt="Github Logo" text="Github Login" />
+    </Flex>
+  );
+};
+
+export default SignUpPage;
